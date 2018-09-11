9e263cd9634e9dfc4e1c01b76faaf160977b8cc4
Geoff Gaudreault <gaudreault@gmail.com>
2018-09-10 18:59:17 -0700

    Working on Force

--- src/physics/Contact.ts
+++ src/physics/Contact.ts
@@ -3,12 +3,14 @@ namespace Physics {
   export class IContact<A,B> {
     itemA:A;
     itemB:B;
+    angle:number;
   }
 
   export class BaseContact<A, B> implements IContact<A, B> {
 
     public itemA:A;
     public itemB:B;
+    public angle:number;
 
     constructor (itemA:A, itemB:B) {
 
@@ -21,10 +23,31 @@ namespace Physics {
 
   export class BodyBodyContact extends BaseContact<Physics.IBody, Physics.IBody> {
 
-  }
+    constructor (itemA:Physics.IBody, itemB:Physics.IBody) {
 
+      super(itemA, itemB);
+
+      let a = itemA.bounds.anchor;
+      let b = itemB.bounds.anchor;
+    
+      this.angle = Geom.angleBetween(a.x, a.y, b.x, b.y);
+
+    }
+
+  }
   
   export class BodyBoundaryContact extends BaseContact<Physics.IBody, Geom.ISegment> {
+
+    constructor (itemA:Physics.IBody, itemB:Geom.ISegment) {
+
+      super(itemA, itemB);
+
+      let a = itemA.bounds.anchor;
+      let b = Geom.closestPtPointLine(a, itemB.ptA, itemB.ptB);
+    
+      this.angle = Geom.angleBetween(a.x, a.y, b.x, b.y);
+
+    }
 
   }
 
--- src/physics/Force.ts
+++ src/physics/Force.ts
@@ -1,15 +1,38 @@
 namespace Physics {
 
   export interface IForce extends Geom.IPoint {
-
+    parentID:number;
   }
 
   export class Force implements IForce {
 
+    public parentID:number;
     public x:number;
     public y:number;
 
-    static fromContact (contact:IContact<IBody, any>):IForce {
+    constructor (x:number = 0, y:number = 0) {
+
+      this.x = x;
+      this.y = y;
+
+    }
+
+    static fromContact (contact:IContact<IBody, any>):Array<IForce> {
+
+      let a = contact.itemA;
+
+      if (contact instanceof BodyBodyContact) {
+
+        let forceA = new Force();
+        let forceB = new Force();
+
+      }
+
+      if (contact instanceof BodyBoundaryContact) {
+
+        let b = contact.itemB as Geom.ISegment;
+
+      }
 
       return null;
 
